e13419ff20d0fa76b15ca1059ccd9668f2e7b8fb
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2023-07-08 20:25:58 +0300

    Optimized a sql requests and added tag searchinng and tag search result page

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,19 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 
 from .models import (
-    Post, 
-    Tag
+    Post,
+    Tag,
 )
 
 
 def index_view(request):
-    posts = Post.objects.all().order_by('-created_at').prefetch_related('author')
+    posts = Post.objects.all().order_by('-created_at') \
+                        .prefetch_related('author').prefetch_related('tags')
+    tags = Tag.objects.all()
+
     context = {
         'posts': posts,
+        'tags': tags,
     }
 
     return render(request, 'blog/index.html', context)
@@ -18,12 +22,14 @@ def index_view(request):
 
 def search_view(request):
     query = request.GET.get('q')
-    posts = Post.objects.filter(Q(title__icontains=query)).all() \
-                        .order_by('-created_at').prefetch_related('author')
+    posts = Post.objects.filter(Q(title__icontains=query)).all().order_by('-created_at') \
+                        .prefetch_related('author').prefetch_related('tags')
+    tags = Tag.objects.all()
 
     context = {
         'posts': posts,
         'search_query': query,
+        'tags': tags,
     }
 
     return render(request, 'blog/index.html', context)
@@ -31,11 +37,14 @@ def search_view(request):
 
 def category_view(request, slug):
     tag = get_object_or_404(Tag, slug=slug)
-    posts = Post.objects.filter(tags=tag).all() \
-                        .order_by('-created_at').prefetch_related('author')
+    posts = Post.objects.filter(tags=tag).all().order_by('-created_at') \
+                        .prefetch_related('author').prefetch_related('tags')
+    tags = Tag.objects.all()
+
     context = {
         'posts': posts,
-        'tag': tag,
+        'tags': tags,
+        'selected_tag': tag,
     }
 
     return render(request, 'blog/index.html', context)
