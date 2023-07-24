c66ed99fc7d5c445973e966b94aee0202856d5e7
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2023-07-24 18:26:49 +0300

    Added default profile image

--- src/account/views.py
+++ src/account/views.py
@@ -10,78 +10,78 @@ logger = logging.getLogger(__name__)
 
 def index_view(request, slug):
     """
-	View function render the account page for an user
-	"""
+    View function render the account page for an user
+    """
     user = request.user
 
     if user.is_authenticated:
         if user.slug == slug:
-            return render(request, 'account/own_account.html')
+            return render(request, "account/own_account.html")
         else:
-            return render(request, 'account/account.html')
+            return render(request, "account/account.html")
     else:
-        return render(request, 'account/account.html')
+        return render(request, "account/account.html")
 
 
 def logout_view(request):
-	"""
-	Function to logout from an account and redirect to the home page
-	"""
-	logout(request)
-	return redirect('home')
+    """
+    Function to logout from an account and redirect to the home page
+    """
+    logout(request)
+    return redirect("home")
 
 
 def login_view(request, *args, **kwargs):
-	"""
+    """
     Function to login as an user and redirect to the account page
     """
-	context = {}
+    context = {}
 
-	user = request.user
-	if user.is_authenticated:
-		return redirect('account', user.slug)
+    user = request.user
+    if user.is_authenticated:
+        return redirect("account", user.slug)
 
-	if request.POST:
-		form = AccountAuthenticationForm(request.POST)
-		if form.is_valid():
-			nickname = form.cleaned_data['nickname']
-			password = form.cleaned_data['password']
-			account = authenticate(nickname=nickname, password=password)
-			if account:
-				login(request, account)
-				logger.info(f'User \'{account}\' has been login')
-				return redirect('account', account.slug)
-	else:
-		form = AccountAuthenticationForm()
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            nickname = form.cleaned_data["nickname"]
+            password = form.cleaned_data["password"]
+            account = authenticate(nickname=nickname, password=password)
+            if account:
+                login(request, account)
+                logger.info(f"User '{account}' has been login")
+                return redirect("account", account.slug)
+    else:
+        form = AccountAuthenticationForm()
 
-	context['login_form'] = form
+    context["login_form"] = form
 
-	return render(request, 'account/login.html', context)
+    return render(request, "account/login.html", context)
 
 
 def register_view(request, *args, **kwargs):
-	"""
-	View function to create and register an account
-	"""
-	context = {}
+    """
+    View function to create and register an account
+    """
+    context = {}
 
-	user = request.user
-	if user.is_authenticated:
-		return redirect('account', user.slug)
+    user = request.user
+    if user.is_authenticated:
+        return redirect("account", user.slug)
 
-	if request.POST:
-		form = AccountRegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			nickname = form.cleaned_data['nickname'].lower()
-			raw_password = form.cleaned_data['password1']
-			account = authenticate(nickname=nickname, password=raw_password)
-			logger.info(f'New user {account} has been registered')
-			login(request, account)
-			return redirect('account', account.slug)
-	else:
-	    form = AccountRegisterForm()
+    if request.POST:
+        form = AccountRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            nickname = form.cleaned_data["nickname"].lower()
+            raw_password = form.cleaned_data["password1"]
+            account = authenticate(nickname=nickname, password=raw_password)
+            logger.info(f"New user {account} has been registered")
+            login(request, account)
+            return redirect("account", account.slug)
+    else:
+        form = AccountRegisterForm()
 
-	context['signup_form'] = form
+    context["signup_form"] = form
 
-	return render(request, 'account/register.html', context)
+    return render(request, "account/register.html", context)
